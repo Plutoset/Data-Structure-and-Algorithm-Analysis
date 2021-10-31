2cbe087933f4f3e1fb0dc4c4bfe2d462a8efda44
Plutoset <1452089861@qq.com>
2021-10-31 23:14:07 +0800

    很劣鸡的kd树，删除算法是重构树。

---
+++ KDTree/KDTree/KDTreeNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KDTree
+{
+    public class KDTreeNode<T>
+    {
+        public bool Axis { get; set; }
+        public Point Position { get; set; }
+        public T Value { get; set; }
+        public KDTreeNode<T> Left { get; set; }
+        public KDTreeNode<T> Right { get; set; }
+
+        public bool GetIsLeaf() => Left == null && Right == null;
+        public KDTreeNode(Point position, T value)
+        {
+            this.Position = position;
+            this.Value = value;
+        }
+    }
+}
